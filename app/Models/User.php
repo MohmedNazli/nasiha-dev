ca836db2d16dba920e865ef71961c72801fdafa9
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-02-06 17:38:16 +0200

    Add new columns tom users table and make table soft deleted

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,17 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+
+    public const ADMIN_USER = 'admin';
+    public const COMPANY_USER = 'company';
 
     /**
      * The attributes that are mass assignable.
