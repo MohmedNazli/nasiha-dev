2095360ddce00a00ef67226f6bd824c4c53de074
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-02-06 16:04:25 +0200

    delete project

--- resources/js/app.js
+++
@@ -1,5 +0,0 @@
-import {createApp} from 'vue'
-import App from "../vue/App.vue";
-import router from "../vue/router";
-
-createApp(App).use(router).mount("#app")
--- resources/vue/pages/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <h1>
-    Hi from the About page!
-  </h1>
-</template>
