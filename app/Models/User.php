80f25eb09e5ef31815f8db7f734a58e1e3a3682b
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-02-07 15:02:16 +0200

    Add roles for users model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,16 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes, HasRoles;
 
     public const ADMIN_USER = 'admin';
     public const COMPANY_USER = 'company';
