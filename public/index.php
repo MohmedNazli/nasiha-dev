f93c82ae67be1adbc86946b232fd1b3223d6760d
Amr Awad <42295110+Awad2002@users.noreply.github.com>
2023-02-07 14:41:58 +0200

    fix public folder

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
+header('Location: public/');
 
 /*
 |--------------------------------------------------------------------------
