87a57d6d65b10461fea55d3a9638d99b7087e0db
Amr Awad <42295110+Awad2002@users.noreply.github.com>
2023-02-07 15:21:13 +0200

    Revert "fix routing"
    This reverts commit ac4729c64bb2643bb562a01277e871e9d0fea948.

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/admin', function () {
     return view('admin.dashboard.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -29,7 +33,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-
-Route::get('/{any}', function () {
-    return view('welcome');
-})->where("any",".*");
