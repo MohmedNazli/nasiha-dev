483f21fd9133673bc5636bca8a69c3a945df1f15
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-02-07 19:48:44 +0200

    Refactor AdminSeeder.php and assign roles for admin

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,23 +5,24 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
-use Spatie\Permission\Models\Role;
 
 class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-        User::query()->updateOrCreate(
+        $user = User::query()->updateOrCreate(
             [
                 'name' => 'Nasiha Admin',
                 'user_type' => User::ADMIN_USER,
                 'email' => 'admin@nasiha.com',
                 'email_verified_at' => now(),
                 'password' => bcrypt('admin@nasiha.com'),
-                'avatar' => asset('assets/img/logo.png'),
+                'avatar' => 'imgs/logo.png',
                 'remember_token' => Str::random(10),
                 'address' => 'Riyadh, Saudi Arabia',
             ]
-        )->assignRole(Role::create(['name' => 'Super-Admin']));
+        );
+        $user->assignRole('super-admin');
+        $user->assignRole(User::ADMIN_USER);
     }
 }
