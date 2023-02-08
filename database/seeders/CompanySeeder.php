6435d0769c353cdd546617216f69b9170881e51a
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-02-08 18:27:03 +0200

    Create table, model, factory and seeder for companies,

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Company;
+use Illuminate\Database\Seeder;
+
+class CompanySeeder extends Seeder
+{
+    public function run(): void
+    {
+        Company::factory()
+            ->count(10)
+            ->create();
+    }
+}
