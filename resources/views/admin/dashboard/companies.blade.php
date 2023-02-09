75742586546218fecd4f2e5d286c7d00b8b5eedb
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-02-09 22:19:41 +0200

    remove unnecessary div

--- resources/views/admin/dashboard/companies.blade.php
+++ resources/views/admin/dashboard/companies.blade.php
@@ -82,100 +82,6 @@
                                 <div class="card-toolbar">
                                     <!--begin::Toolbar-->
                                     <div class="d-flex justify-content-end" data-kt-user-table-toolbar="base">
-                                        <!--begin::Filter-->
-                                        <button type="button" class="btn btn-light-primary me-3"
-                                                data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-                                            <!--begin::Svg Icon | path: icons/duotune/general/gen031.svg-->
-                                            <span class="svg-icon svg-icon-2">
-														<svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                                             xmlns="http://www.w3.org/2000/svg">
-															<path d="M19.0759 3H4.72777C3.95892 3 3.47768 3.83148 3.86067 4.49814L8.56967 12.6949C9.17923 13.7559 9.5 14.9582 9.5 16.1819V19.5072C9.5 20.2189 10.2223 20.7028 10.8805 20.432L13.8805 19.1977C14.2553 19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5 12.8089 14.8171 11.8056 15.408 10.964L19.8943 4.57465C20.3596 3.912 19.8856 3 19.0759 3Z"
-                                                                  fill="currentColor"/>
-														</svg>
-													</span>
-                                            <!--end::Svg Icon-->Filter
-                                        </button>
-                                        <!--begin::Menu 1-->
-                                        <div class="menu menu-sub menu-sub-dropdown w-300px w-md-325px"
-                                             data-kt-menu="true">
-                                            <!--begin::Header-->
-                                            <div class="px-7 py-5">
-                                                <div class="fs-5 text-dark fw-bold">Filter Options</div>
-                                            </div>
-                                            <!--end::Header-->
-                                            <!--begin::Separator-->
-                                            <div class="separator border-gray-200"></div>
-                                            <!--end::Separator-->
-                                            <!--begin::Content-->
-                                            <div class="px-7 py-5" data-kt-user-table-filter="form">
-                                                <!--begin::Input group-->
-                                                <div class="mb-10">
-                                                    <label class="form-label fs-6 fw-semibold">Role:</label>
-                                                    <select class="form-select form-select-solid fw-bold"
-                                                            data-kt-select2="true" data-placeholder="Select option"
-                                                            data-allow-clear="true" data-kt-user-table-filter="role"
-                                                            data-hide-search="true">
-                                                        <option></option>
-                                                        <option value="Administrator">Administrator</option>
-                                                        <option value="Analyst">Analyst</option>
-                                                        <option value="Developer">Developer</option>
-                                                        <option value="Support">Support</option>
-                                                        <option value="Trial">Trial</option>
-                                                    </select>
-                                                </div>
-                                                <!--end::Input group-->
-                                                <!--begin::Input group-->
-                                                <div class="mb-10">
-                                                    <label class="form-label fs-6 fw-semibold">Two Step
-                                                        Verification:</label>
-                                                    <select class="form-select form-select-solid fw-bold"
-                                                            data-kt-select2="true" data-placeholder="Select option"
-                                                            data-allow-clear="true"
-                                                            data-kt-user-table-filter="two-step"
-                                                            data-hide-search="true">
-                                                        <option></option>
-                                                        <option value="Enabled">Enabled</option>
-                                                    </select>
-                                                </div>
-                                                <!--end::Input group-->
-                                                <!--begin::Actions-->
-                                                <div class="d-flex justify-content-end">
-                                                    <button type="reset"
-                                                            class="btn btn-light btn-active-light-primary fw-semibold me-2 px-6"
-                                                            data-kt-menu-dismiss="true"
-                                                            data-kt-user-table-filter="reset">Reset
-                                                    </button>
-                                                    <button type="submit" class="btn btn-primary fw-semibold px-6"
-                                                            data-kt-menu-dismiss="true"
-                                                            data-kt-user-table-filter="filter">Apply
-                                                    </button>
-                                                </div>
-                                                <!--end::Actions-->
-                                            </div>
-                                            <!--end::Content-->
-                                        </div>
-                                        <!--end::Menu 1-->
-                                        <!--end::Filter-->
-                                        <!--begin::Export-->
-                                        <button type="button" class="btn btn-light-primary me-3"
-                                                data-bs-toggle="modal" data-bs-target="#kt_modal_export_users">
-                                            <!--begin::Svg Icon | path: icons/duotune/arrows/arr078.svg-->
-                                            <span class="svg-icon svg-icon-2">
-														<svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                                             xmlns="http://www.w3.org/2000/svg">
-															<rect opacity="0.3" x="12.75" y="4.25" width="12" height="2"
-                                                                  rx="1" transform="rotate(90 12.75 4.25)"
-                                                                  fill="currentColor"/>
-															<path d="M12.0573 6.11875L13.5203 7.87435C13.9121 8.34457 14.6232 8.37683 15.056 7.94401C15.4457 7.5543 15.4641 6.92836 15.0979 6.51643L12.4974 3.59084C12.0996 3.14332 11.4004 3.14332 11.0026 3.59084L8.40206 6.51643C8.0359 6.92836 8.0543 7.5543 8.44401 7.94401C8.87683 8.37683 9.58785 8.34458 9.9797 7.87435L11.4427 6.11875C11.6026 5.92684 11.8974 5.92684 12.0573 6.11875Z"
-                                                                  fill="currentColor"/>
-															<path opacity="0.3"
-                                                                  d="M18.75 8.25H17.75C17.1977 8.25 16.75 8.69772 16.75 9.25C16.75 9.80228 17.1977 10.25 17.75 10.25C18.3023 10.25 18.75 10.6977 18.75 11.25V18.25C18.75 18.8023 18.3023 19.25 17.75 19.25H5.75C5.19772 19.25 4.75 18.8023 4.75 18.25V11.25C4.75 10.6977 5.19771 10.25 5.75 10.25C6.30229 10.25 6.75 9.80228 6.75 9.25C6.75 8.69772 6.30229 8.25 5.75 8.25H4.75C3.64543 8.25 2.75 9.14543 2.75 10.25V19.25C2.75 20.3546 3.64543 21.25 4.75 21.25H18.75C19.8546 21.25 20.75 20.3546 20.75 19.25V10.25C20.75 9.14543 19.8546 8.25 18.75 8.25Z"
-                                                                  fill="currentColor"/>
-														</svg>
-													</span>
-                                            <!--end::Svg Icon-->Export
-                                        </button>
-                                        <!--end::Export-->
                                         <!--begin::Add user-->
                                         <button type="button" class="btn btn-primary" data-bs-toggle="modal"
                                                 data-bs-target="#kt_modal_add_user">
@@ -207,109 +113,6 @@
                                         </button>
                                     </div>
                                     <!--end::Group actions-->
-                                    <!--begin::Modal - Adjust Balance-->
-                                    <div class="modal fade" id="kt_modal_export_users" tabindex="-1"
-                                         aria-hidden="true">
-                                        <!--begin::Modal dialog-->
-                                        <div class="modal-dialog modal-dialog-centered mw-650px">
-                                            <!--begin::Modal content-->
-                                            <div class="modal-content">
-                                                <!--begin::Modal header-->
-                                                <div class="modal-header">
-                                                    <!--begin::Modal title-->
-                                                    <h2 class="fw-bold">Export Users</h2>
-                                                    <!--end::Modal title-->
-                                                    <!--begin::Close-->
-                                                    <div class="btn btn-icon btn-sm btn-active-icon-primary"
-                                                         data-kt-users-modal-action="close">
-                                                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
-                                                        <span class="svg-icon svg-icon-1">
-																		<svg width="24" height="24" viewBox="0 0 24 24"
-                                                                             fill="none"
-                                                                             xmlns="http://www.w3.org/2000/svg">
-																			<rect opacity="0.5" x="6" y="17.3137"
-                                                                                  width="16" height="2" rx="1"
-                                                                                  transform="rotate(-45 6 17.3137)"
-                                                                                  fill="currentColor"/>
-																			<rect x="7.41422" y="6" width="16"
-                                                                                  height="2" rx="1"
-                                                                                  transform="rotate(45 7.41422 6)"
-                                                                                  fill="currentColor"/>
-																		</svg>
-																	</span>
-                                                        <!--end::Svg Icon-->
-                                                    </div>
-                                                    <!--end::Close-->
-                                                </div>
-                                                <!--end::Modal header-->
-                                                <!--begin::Modal body-->
-                                                <div class="modal-body scroll-y mx-5 mx-xl-15 my-7">
-                                                    <!--begin::Form-->
-                                                    <form id="kt_modal_export_users_form" class="form" action="#">
-                                                        <!--begin::Input group-->
-                                                        <div class="fv-row mb-10">
-                                                            <!--begin::Label-->
-                                                            <label class="fs-6 fw-semibold form-label mb-2">Select
-                                                                Roles:</label>
-                                                            <!--end::Label-->
-                                                            <!--begin::Input-->
-                                                            <select name="role" data-control="select2"
-                                                                    data-placeholder="Select a role"
-                                                                    data-hide-search="true"
-                                                                    class="form-select form-select-solid fw-bold">
-                                                                <option></option>
-                                                                <option value="Administrator">Administrator</option>
-                                                                <option value="Analyst">Analyst</option>
-                                                                <option value="Developer">Developer</option>
-                                                                <option value="Support">Support</option>
-                                                                <option value="Trial">Trial</option>
-                                                            </select>
-                                                            <!--end::Input-->
-                                                        </div>
-                                                        <!--end::Input group-->
-                                                        <!--begin::Input group-->
-                                                        <div class="fv-row mb-10">
-                                                            <!--begin::Label-->
-                                                            <label class="required fs-6 fw-semibold form-label mb-2">Select
-                                                                Export Format:</label>
-                                                            <!--end::Label-->
-                                                            <!--begin::Input-->
-                                                            <select name="format" data-control="select2"
-                                                                    data-placeholder="Select a format"
-                                                                    data-hide-search="true"
-                                                                    class="form-select form-select-solid fw-bold">
-                                                                <option></option>
-                                                                <option value="excel">Excel</option>
-                                                                <option value="pdf">PDF</option>
-                                                                <option value="cvs">CVS</option>
-                                                                <option value="zip">ZIP</option>
-                                                            </select>
-                                                            <!--end::Input-->
-                                                        </div>
-                                                        <!--end::Input group-->
-                                                        <!--begin::Actions-->
-                                                        <div class="text-center">
-                                                            <button type="reset" class="btn btn-light me-3"
-                                                                    data-kt-users-modal-action="cancel">Discard
-                                                            </button>
-                                                            <button type="submit" class="btn btn-primary"
-                                                                    data-kt-users-modal-action="submit">
-                                                                <span class="indicator-label">Submit</span>
-                                                                <span class="indicator-progress">Please wait...
-																			<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
-                                                            </button>
-                                                        </div>
-                                                        <!--end::Actions-->
-                                                    </form>
-                                                    <!--end::Form-->
-                                                </div>
-                                                <!--end::Modal body-->
-                                            </div>
-                                            <!--end::Modal content-->
-                                        </div>
-                                        <!--end::Modal dialog-->
-                                    </div>
-                                    <!--end::Modal - New Card-->
                                     <!--begin::Modal - Add task-->
                                     <div class="modal fade" id="kt_modal_add_user" tabindex="-1" aria-hidden="true">
                                         <!--begin::Modal dialog-->
@@ -578,7 +381,7 @@
                                                      data-kt-menu="true">
                                                     <!--begin::Menu item-->
                                                     <div class="menu-item px-3">
-                                                        <a href="../../demo1/dist/apps/user-management/users/view.html"
+                                                        <a href="#"
                                                            class="menu-link px-3">Edit</a>
                                                     </div>
                                                     <!--end::Menu item-->
@@ -906,7 +709,6 @@
 <!--end::Vendors Javascript-->
 <!--begin::Custom Javascript(used for this page only)-->
 <script src="admin-panel/js/custom/apps/user-management/users/list/table.js"></script>
-<script src="admin-panel/js/custom/apps/user-management/users/list/export-users.js"></script>
 <script src="admin-panel/js/custom/apps/user-management/users/list/add.js"></script>
 <script src="admin-panel/js/widgets.bundle.js"></script>
 <script src="admin-panel/js/custom/apps/chat/chat.js"></script>
