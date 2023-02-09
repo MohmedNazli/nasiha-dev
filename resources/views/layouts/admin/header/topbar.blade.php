fd3f0c24f72958cdbf06e37045a48abb1449c4f6
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-02-09 13:25:36 +0200

    Edit image

--- resources/views/layouts/admin/header/topbar.blade.php
+++ resources/views/layouts/admin/header/topbar.blade.php
@@ -84,7 +84,7 @@
     <div class="d-flex align-items-center ms-1 ms-lg-3" id="kt_header_user_menu_toggle">
       <!--begin::Menu wrapper-->
       <div class="cursor-pointer symbol symbol-30px symbol-md-40px" data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-start" data-kt-menu-flip="bottom">
-        <img src="{{asset(auth()->user()->avatar ?? 'images/avatar.jpeg')}}" alt="metronic" />
+        <img src="{{asset(auth()->user()->image ?? 'images/avatar.jpeg')}}" alt="metronic" />
       </div>
       <!--begin::Menu-->
       <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px" data-kt-menu="true">
@@ -93,14 +93,14 @@
           <div class="menu-content d-flex align-items-center px-3">
             <!--begin::Avatar-->
             <div class="symbol symbol-50px me-5">
-              <img alt="Logo" src="{{asset(auth()->user()->avatar ?? 'images/avatar.jpeg')}}" />
+              <img alt="Logo" src="{{asset(auth()->user()->image ?? 'images/avatar.jpeg')}}" />
             </div>
             <!--end::Avatar-->
             <!--begin::Username-->
             <div class="d-flex flex-column">
-              <div class="fw-bolder d-flex align-items-center fs-5">{{auth()->user()->name}}
+              <div class="fw-bolder d-flex align-items-center fs-5">{{auth()->user()->name ?? ''}}
               </div>
-              <a href="#" class="fw-bold text-muted text-hover-primary fs-7" dir="ltr">{{auth()->user()->email}}</a>
+              <a href="#" class="fw-bold text-muted text-hover-primary fs-7" dir="ltr">{{auth()->user()->email ?? ''}}</a>
             </div>
             <!--end::Username-->
           </div>
