232d063f62c1231fd9ec7a20a87d800ca9bba588
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-02-09 18:18:31 +0200

    Create new panel for company

---
+++ resources/views/company/dashboard/breadcrumb.blade.php
@@ -0,0 +1,19 @@
+<div class="toolbar" id="kt_toolbar">
+  <!--begin::Container-->
+  <div id="kt_toolbar_container" class="container-fluid d-flex flex-stack">
+    <!--begin::Page title-->
+    <div data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}" class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
+      <!--begin::Title-->
+      <h1 class="d-flex align-items-center text-dark fw-bolder fs-3 my-1">
+        لوحة التحكم
+      <!--begin::Separator-->
+      <span class="h-20px border-gray-200 border-start ms-3 mx-2"></span>
+      <!--end::Separator-->
+    </h1>
+      <!--end::Title-->
+    </div>
+    <!--end::Page title-->
+
+  </div>
+  <!--end::Container-->
+</div>
