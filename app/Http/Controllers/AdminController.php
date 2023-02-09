90037be859a52a0aa4d8b33f5e77b6d077730c5b
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-02-09 10:32:32 +0200

    create company

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rules;
+
+
 class AdminController extends Controller
 {
 
@@ -12,6 +21,30 @@ class AdminController extends Controller
 
     public function companies()
     {
-        return view('admin.dashboard.companies');
+        $companies = User::typeCustomer()->get();
+        return view('admin.dashboard.companies', compact('companies'));
+    }
+
+
+    public function storeCompany(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'user_type' => User::COMPANY_USER,
+            'email_verified_at' => now(),
+            'password' => Hash::make($request->password),
+            'avatar' => 'images/logo.png',
+        ]);
+
+        event(new Registered($user));
+
+        return back();
     }
 }
