7543b054f15c878f14afaafaa5712ce7de5806ea
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-02-09 12:05:21 +0200

    revert laravel ui

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
