ccda7ca35af03d310fa26970de197be53b3b907a
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-02-09 22:41:57 +0200

    Create new controller for companies

---
+++ app/Http/Controllers/Panel/Company/EmployeeController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Panel\Company;
+
+use App\Http\Controllers\Controller;
+use App\Models\Account;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Application|Factory|View
+     */
+    public function index()
+    {
+        $employees = Account::all();
+        return view('company.dashboard.employees', compact('employees'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.Account::class],
+            'password' => ['required'],
+        ]);
+
+
+        $user = Account::create([
+            'company_id' => auth()->user()?->id,
+            'name' => $request->name,
+            'job_title' => $request?->job_title,
+            'email' => $request->email,
+            'mobile' => $request?->mobile,
+            'password' => Hash::make($request->password),
+            'image' => 'images/logo.png',
+        ]);
+
+        event(new Registered($user));
+
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
