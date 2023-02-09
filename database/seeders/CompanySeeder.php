b90d321037c2f5b78cedd18b704fa73a6ae092e7
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-02-09 18:15:58 +0200

    Edit Company seeder

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -4,13 +4,22 @@ namespace Database\Seeders;
 
 use App\Models\Company;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class CompanySeeder extends Seeder
 {
     public function run(): void
     {
-        Company::factory()
-            ->count(10)
-            ->create();
+        Company::query()->create(
+            [
+                'name' => 'Nasiha Company',
+                'user_name' => 'nasiha_company',
+                'email' => 'company@nasiha.com',
+                'password' => Hash::make('company@nasiha.com'),
+                'image' => 'images/logo.png',
+                'isActive' => true,
+                'settings' => '["color","red"]',
+            ]
+        );
     }
 }
